acbe402794ccc45e25b37c869d517ddb0930e35f
Manoj Kumar <44832007+manukudur@users.noreply.github.com>
2019-09-09 13:24:04 +0530

    dotenv

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-// const DB_URL =
-//   "mongodb+srv://guru:guru123@crudcluster-4tslz.mongodb.net/test?retryWrites=true&w=majority";
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || "3000";
